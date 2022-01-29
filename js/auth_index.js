41fca4404dbdb28e9f3c2e28aa1757b1a97638e7
Ilyaguar <ilyaguar@gmail.com>
2022-01-29 19:33:22 +0400

    track list upgraded

--- js/auth_index.js
+++ js/auth_index.js
@@ -14,6 +14,69 @@ function enableScroll() {
     window.onscroll = function() {};
 }
 
+function trackList(album_id, offset) {
+    let xhr = new XMLHttpRequest();
+    let r_link = baseUrl + '/playlists/' + album_id + '/tracks?offset=' + offset;
+    let total = 0
+    let res
+    
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+
+            res = xhr.responseText;
+            res = JSON.parse(res)
+            console.log(res)
+            $('#album_info')[0].children[0].innerText = res.name
+
+            total = res.total
+
+            i = 1 + offset;
+            res.items.forEach(element => {
+                let cover = element.track.album.images[0].url,
+                    name = element.track.name;
+                    artists = []
+                    duration_sec = Math.floor((element.track.duration_ms)/1000)
+                    duration = (Math.floor(duration_sec/60)) + ':' + new Intl.NumberFormat('ru-RU', {minimumIntegerDigits: 2}).format(duration_sec%60);
+
+                element.track.artists.forEach(element => {
+                    artists.push(element.name)
+                });
+
+                artists = artists.join(', ')
+
+                if (name.length > 30){
+                    name = name.substring(0, 28)
+                    name += '...'
+                }
+
+                let block = `
+                    <div class="track">
+                        <span>${i}</span>
+                        <img src="${cover}" alt="cover">
+                        <div class="track_info">
+                            <span>${name}</span>
+                            <br>
+                            <span class="artists">${artists}</span>
+                        </div>
+                        <span>${duration}</span>
+                    </div>
+                `;
+                $('.track_list').append(block);
+                i = i + 1;
+            });
+        }
+    }
+
+    xhr.open('GET', r_link, false);
+    
+    xhr.setRequestHeader('Content-Type', 'application/json')
+    xhr.setRequestHeader('Authorization', 'Bearer ' + token)
+    
+    xhr.send();
+
+    return(total)
+}
+
 /*----------------------------------------------------------------------------------------*/
 
 $('#player').hide(),
@@ -94,32 +157,32 @@ cards.forEach(element => {
 
 /*---Dev_process----------------------------------------------------------------------------*/
 
-// $('#submitSrch').on('click', function(){
-//     let cards = Array.from($('.card'))
-//     let card_covers = Array.from($('.card_cover'))
+$('#submitSrch').on('click', function(){
+    let cards = Array.from($('.card'))
+    let card_covers = Array.from($('.card_cover'))
 
-//     i = 0;
-//     cards.forEach(element => {
-//         let xhr = new XMLHttpRequest();
-//         let r_link = baseUrl + '/playlists/' + element.id;
+    i = 0;
+    cards.forEach(element => {
+        let xhr = new XMLHttpRequest();
+        let r_link = baseUrl + '/playlists/' + element.id;
         
-//         xhr.onreadystatechange = function() {
-//             if (xhr.readyState == XMLHttpRequest.DONE) {
-//                 let res = xhr.responseText;
-//                 res = JSON.parse(res)
-//                 card_covers[i].src = res.images[0].url
-//                 i = i + 1;
-//             }
-//         }
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                let res = xhr.responseText;
+                res = JSON.parse(res)
+                card_covers[i].src = res.images[0].url
+                i = i + 1;
+            }
+        }
 
-//         xhr.open('GET', r_link, false);
+        xhr.open('GET', r_link, false);
             
-//         xhr.setRequestHeader('Content-Type', 'application/json')
-//         xhr.setRequestHeader('Authorization', 'Bearer ' + token)
+        xhr.setRequestHeader('Content-Type', 'application/json')
+        xhr.setRequestHeader('Authorization', 'Bearer ' + token)
             
-//         xhr.send();
-//     })
-// })
+        xhr.send();
+    })
+})
 
 /*----------------------------------------------------------------------------------------*/
 
@@ -132,108 +195,16 @@ $('.card').on('click', function(){
 
     $('#album_info')[0].children[1].attributes[0].value = album_cover
 
-    var xhr = new XMLHttpRequest();
-    let r_link = baseUrl + '/playlists/' + album_id;
+    let offset = 0
     
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-
-            let res = xhr.responseText;
-            res = JSON.parse(res)
-            $('#album_info')[0].children[0].innerText = res.name
-
-            res.tracks.items.forEach(element => {
-                let cover = element.track.album.images[0].url,
-                    name = element.track.name;
-                    artists = []
-                    duration_sec = Math.floor((element.track.duration_ms)/1000)
-                    duration = (Math.floor(duration_sec/60)) + ':' + new Intl.NumberFormat('ru-RU', {minimumIntegerDigits: 2}).format(duration_sec%60);
-
-                element.track.artists.forEach(element => {
-                    artists.push(element.name)
-                });
-
-                artists = artists.join(', ')
-
-                let block = `
-                    <div class="track">
-                        <img src="${cover}" alt="cover">
-                        <div class="track_info">
-                            <span>${name}</span>
-                            <br>
-                            <span class="artists">${artists}</span>
-                        </div>
-                        <span>${duration}</span>
-                    </div>
-                `;
-                $('.track_list').append(block);
-            });
+    while(true){
+        let total = trackList(album_id, offset);
+        
+        if (total > 100){
+            offset = offset + 100;
+        }
+        if (offset > total){
+            break
         }
     }
-
-    xhr.open('GET', r_link, true);
-    
-    xhr.setRequestHeader('Content-Type', 'application/json')
-    xhr.setRequestHeader('Authorization', 'Bearer ' + token)
-    
-    xhr.send();
-
 })
-
-// $('#pause').on('click', function() {
-//     let trackid = '5hheGdf1cb4rK0FNiedCfK'
-    
-//     var xhr = new XMLHttpRequest();
-//     let r_link = baseUrl + '/tracks/3NqBxTOMCJ3zW9CIP51td4';
-//     let res = 'nope'
-
-//     xhr.onreadystatechange = function() {
-//         if (xhr.readyState == XMLHttpRequest.DONE) {
-//             res = xhr.responseText;
-//         }
-//     }
-//     // 2. Конфигурируем его: GET-запрос на URL 'phones.json'
-//     xhr.open('GET', r_link, true);
-
-//     xhr.setRequestHeader('Content-Type', 'application/json')
-//     xhr.setRequestHeader('Authorization', 'Bearer ' + token)
-
-//     $('#pause').attr('id', 'play')
-
-//     // 3. Отсылаем запрос
-//     xhr.send();
-// });
-
-
-// $('#prev').on('click', function() { 
-//     var xhr = new XMLHttpRequest();
-//     let r_link = baseUrl + '/me/player/shuffle';
-
-//     // 2. Конфигурируем его: GET-запрос на URL 'phones.json'
-//     xhr.open('PUT', r_link, true);
-
-//     xhr.setRequestHeader('Authorization', 'Bearer ' + token);
-//     xhr.setRequestHeader('Content-Type', 'application/json');
-
-//     // 3. Отсылаем запрос
-//     xhr.send();
-// });
-
-
-// $('#play').on('click', function() {
-//     let trackid = '5hheGdf1cb4rK0FNiedCfK'
-    
-//     var xhr = new XMLHttpRequest();
-//     let r_link = baseUrl + '/me/player/pause';
-
-//     // 2. Конфигурируем его: GET-запрос на URL 'phones.json'
-//     xhr.open('PUT', r_link, true);
-
-//     xhr.setRequestHeader('Content-Type', 'application/json')
-//     xhr.setRequestHeader('Authorization', 'Bearer ' + token)
-
-//     $('#play').attr('id', 'pause')
-    
-//     // 3. Отсылаем запрос
-//     xhr.send();
-// })
